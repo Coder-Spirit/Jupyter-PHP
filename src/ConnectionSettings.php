4d5c50b14bde3ebec672a56c83eafd1516533690
Andreu Correa Casablanca <castarco@gmail.com>
2016-04-06 00:01:54 +0200

    Added licensing code comments

--- src/ConnectionSettings.php
+++ src/ConnectionSettings.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of Jupyter-PHP.
+ *
+ * (c) 2015-2016 Litipk
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Litipk\JupyterPHP;
 
